6d23a06f9e4c3c7f2b693db3431a5735b6dadcae
myselfims <shaikhimran7585@gmail.com>
2023-10-19 16:21:37 +0530

    actor detail anchor added

--- src/pages/Details/artistCard/ArtistCard.jsx
+++ src/pages/Details/artistCard/ArtistCard.jsx
@@ -6,11 +6,14 @@ const ArtistCard = ({item}) => {
 
   
   return (
+    <a href={`https://www.google.com/search?q=${item.name}`}>
     <div className='artistCard'>
       <Img image={{src:`${item.profile_path?`https://image.tmdb.org/t/p/original${item.profile_path}`:'https://cdn2.vectorstock.com/i/1000x1000/88/26/no-image-available-icon-flat-vector-25898826.jpg'}`}}/>
       <h5>{item.name}</h5>
       <h5 className='charName'>{item.character}</h5>
     </div>
+
+    </a>
   )
 }
 
